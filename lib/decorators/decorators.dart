795596348d9614d2bd8b6b54fd940865e54bd020
Zachary Huff <zach.huff.386@gmail.com>
2015-02-18 04:41:12 -0500

    Add polymer decorator

--- lib/decorators/decorators.dart
+++ lib/decorators/decorators.dart
@@ -1,11 +1,13 @@
 library decorators;
 
 import 'package:boilerplate/decorators/noselect.dart' as noselect;
+import 'package:boilerplate/decorators/polymer.dart' as polymer;
 
 import 'package:angular/angular.dart' as ng;
 
 class DecoratorsMod extends ng.Module {
   DecoratorsMod() {
     this.bind(noselect.NoSelectDec);
+    this.bind(polymer.PolymerDec);
   }
 }
