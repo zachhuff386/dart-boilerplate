ec61f81a1d53610a8abc0515d0aff0842404bc5c
Zachary Huff <zach.huff.386@gmail.com>
2015-02-21 13:53:27 -0500

    Remove settings from router

--- lib/routers/main.dart
+++ lib/routers/main.dart
@@ -1,7 +1,5 @@
 library main_rout;
 
-import 'package:boilerplate/settings/settings.dart' as settings;
-
 import 'package:angular/angular.dart' as ng;
 
 MainRout(ng.Router router, ng.RouteViewFactory views) {
@@ -10,7 +8,6 @@ MainRout(ng.Router router, ng.RouteViewFactory views) {
       path: '/root',
       view: 'packages/boilerplate/views/root.html',
       enter: (_) {
-        settings.set('active_page', 'root');
       },
       defaultRoute: true
     )
