26cab6578c0a4ff3d8d5ae93e8f253d622ccb386
Zachary Huff <zach.huff.386@gmail.com>
2015-05-03 17:19:28 -0400

    Return model on add

--- lib/collection.dart
+++ lib/collection.dart
@@ -69,9 +69,9 @@ abstract class Collection extends remote.Remote with collection.IterableMixin {
     return false;
   }
 
-  void append(dynamic data) {
+  model.Model append(dynamic data) {
     if (this.update(data)) {
-      return;
+      return null;
     }
 
     var model = this.newModel();
@@ -79,11 +79,13 @@ abstract class Collection extends remote.Remote with collection.IterableMixin {
     model.init();
     model.import(data);
     this._collection.add(model);
+
+    return model;
   }
 
-  void prepend(dynamic data) {
+  model.Model prepend(dynamic data) {
     if (this.update(data)) {
-      return;
+      return null;
     }
 
     var model = this.newModel();
@@ -91,6 +93,8 @@ abstract class Collection extends remote.Remote with collection.IterableMixin {
     model.init();
     model.import(data);
     this._collection.insert(0, model);
+
+    return model;
   }
 
   bool remove(String id) {
